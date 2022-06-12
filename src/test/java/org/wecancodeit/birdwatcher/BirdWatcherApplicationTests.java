56ca0ce1fed4324d377133b41cba7adae585ec83
AlanKostrick <31017085+AlanKostrick@users.noreply.github.com>
2022-06-12 16:29:08 -0400

    Initial commit

---
+++ src/test/java/org/wecancodeit/birdwatcher/BirdWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.birdwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
