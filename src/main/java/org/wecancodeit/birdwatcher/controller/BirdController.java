954810001c9deb414c665991732a965ac3605e6a
noellegisewhite <noellegisewhite@gmail.com>
2022-06-18 21:14:31 -0400

    bird page style

--- src/main/java/org/wecancodeit/birdwatcher/controller/BirdController.java
+++ src/main/java/org/wecancodeit/birdwatcher/controller/BirdController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.birdwatcher.repository.BirdRepository;
 
 import javax.annotation.Resource;
@@ -24,5 +25,23 @@ public class BirdController {
     public String displaySingleBirds(@PathVariable Long id, Model model) {
         model.addAttribute("singleBird", birdRepo.findById(id));
         return ("single-bird-template.html");
+    }
+
+    @RequestMapping("/bird/country")
+    public String displayBirdByCountry(@PathVariable String country, Model model) {
+        model.addAttribute("countryBird", birdRepo.findByCountry(country));
+        return ("all-birds-template.html");
+    }
+
+    @RequestMapping("/bird/region")
+    public String displayBirdByRegion(@PathVariable String region, Model model) {
+        model.addAttribute("regionBird", birdRepo.findByCountryRegion(region));
+        return ("all-birds-template.html");
+    }
+
+    @RequestMapping("/bird/habitat")
+    public String displayBirdByHabitat(@PathVariable String habitat, Model model) {
+        model.addAttribute("habitatBird", birdRepo.findByHabitat(habitat));
+        return ("all-birds-template.html");
     }
 }
